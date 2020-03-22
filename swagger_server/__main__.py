3841fab64475ed76e77fe5a27fd3d5fd4666260f
johannes.jungbluth <johannesjungbluth@gmail.com>
2020-03-22 11:02:36 +0100

    Fixes flask cors import origin

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,23 +4,14 @@ import connexion
 
 from swagger_server import encoder
 
-try:
-    from flask.ext.cors import CORS  # The typical way to import flask-cors
-except ImportError:
-    # Path hack allows examples to be run without installation.
-    import os
-
-    parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    os.sys.path.insert(0, parentdir)
-
-    from flask.ext.cors import CORS
+from flask_cors import CORS
 
 
 def main():
     app = connexion.App(__name__, specification_dir="./swagger/")
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api("swagger.yaml", arguments={"title": "Corona Extrapolation Data"}, pythonic_params=True)
-    CORS(app)
+    CORS(app.app)
     app.run(port=8080)
 
 
